ef1a8b411c0ea93ae6969b68426074301de7bf32
Mustafa Berkay Mutlu <mustafaberkaymutlu@gmail.com>
2018-06-28 21:57:22 +0200

    Add library dependencies to app module

---
+++ app/src/main/java/net/epictimes/reddit/util/MyGlideModule.java
@@ -0,0 +1,9 @@
+package net.epictimes.reddit.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyGlideModule extends AppGlideModule {
+    // no-op but necessary to keep the class
+}
